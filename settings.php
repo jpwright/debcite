9d6c55ac2ab2eb8ed72e50410afad03a088b43f4
Jason Wright <jpwright0@gmail.com>
2012-03-09 16:49:17 -0500

    Update settings.php

--- settings.php
+++ settings.php
@@ -2,12 +2,13 @@
 
 $cardError = "Sorry, no text found.";
 $fourohfour = "Warning: Connection error to one or more parsed URLs.";
-$yahooAppId = "Auqs5BPV34EXdfh_OxxPwVh_zgR9wS6xgp_42H.cDzD.dRH9zh7rn494YgPd2LCC5g--";
-$googleApiKey = "ABQIAAAAVaLcMOd1_dDmAyUxr1uTDhTJTTOl6G-lE8CPAouRTMn_osvQNhQqiip15m2ZkePCzJ2ez_bBwnx7Mg";
 
-$username="dbo332506237";
-$password="debatecite";
-$database="db332506237";
-$sqlserver="db2488.perfora.net";
+//Private
+$yahooAppId = "(hidden)";
+$googleApiKey = "(hidden)";
+$username="(hidden)";
+$password="(hidden)";
+$database="(hidden)";
+$sqlserver="(hidden)";
 
 ?>
